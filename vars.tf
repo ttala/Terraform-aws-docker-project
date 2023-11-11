32e3397043a40e7b91e96d9f1482c987c82ab45c
ttyeri <tyeri.tala@gmail.com>
2023-11-11 10:13:01 +0100

    Update vars file

---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "REGION" {
+  default = "eu-west-3"
+}
+
+variable "ZONE" {
+  default = "eu-west-3a"
+}
+
+variable "BUCKET" {
+  default = "ttyeri-devops-01"
+}
+
+variable "KEY" {
+  default = "terraform/game42"
+}
